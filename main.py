5a5d5bc589aef63a3ddd46db1bdf570941cc0775
Damian Kasyan <damian.kasyan@hotmail.com>
2020-09-14 10:15:52 +0200

    Add gitignore

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def asking():
+
+    choice_number = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+    first_number = input("Podaj pierwszą liczbę ")
+    second_number = input("Podaj drugą liczbę ")
+    logging.info(f"Pobrano działanie {choice_number} Pierwsza liczba to {first_number} druga liczba to {second_number}")
+
+
+asking()
