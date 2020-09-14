0f55586ecc3f88287fd72eacb1e8f37e2bb784da
Damian Kasyan <damian.kasyan@hotmail.com>
2020-09-14 12:05:27 +0200

    To fix

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def asking():
     choice_number = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
     first_number = input("Podaj pierwszą liczbę ")
     second_number = input("Podaj drugą liczbę ")
-    logging.info(f"Pobrano działanie {choice_number} Pierwsza liczba to {first_number} druga liczba to {second_number}")
+    logging.debug(f"Pobrano działanie {choice_number} Pierwsza liczba to {first_number} druga liczba to {second_number}")
+    result = (choice_number, first_number, second_number)
+    return result
 
 
 asking()
+print(result)
