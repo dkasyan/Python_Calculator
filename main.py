10adf99331fcd6dd55263cd63b5570c7836f529a
Damian Kasyan <damian.kasyan@hotmail.com>
2020-09-20 11:25:16 +0200

    removal def

--- main.py
+++ main.py
@@ -9,9 +9,42 @@ def asking():
     first_number = input("Podaj pierwszą liczbę ")
     second_number = input("Podaj drugą liczbę ")
     logging.debug(f"Pobrano działanie {choice_number} Pierwsza liczba to {first_number} druga liczba to {second_number}")
-    result = (choice_number, first_number, second_number)
-    return result
+
+#Da się zagnieździć funkcje we funkcji ?
+    if choice_number == 1:
+        adding(first_number, second_number)
+    elif choice_number == 2:        
+        removal(first_number, second_number)
+
+    return choice_number, first_number, second_number
+
+def adding(first_number, second_number):
+    new_number = sum(first_number, second_number)
+    logging.debug(f"Dodaję {first_number} i {second_number}")
+    print(new_number)
+
+    return(new_number)
+
+def removal(first_number, second_number):
+    new_number = first_number - second_number
+    logging.debug(f"Odejmuję {first_number} i {second_number}")
+    print(new_number)
+    return new_number
+
+#asking()
+
+result_asking = asking()
+#print(type(result_asking[1]))
+if int(result_asking[0]) == 1:
+    print("dupa")
+    adding(result_asking[1], result_asking[2])
+elif int(result_asking[0]) == 2:
+    print("dupa2")
+
+#def adding (asking()):
 
 
-asking()
-print(result)
+
+
+
+
